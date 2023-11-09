09012b7f97fd128974f1119a45e3f2674cf99713
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2023-11-09 10:45:44 +0100

    Vinterprojektet

---
+++ Vinterprojektet/Program.cs
@@ -0,0 +1,15 @@
+﻿using Raylib_cs;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+
+
+Raylib.InitWindow(1000, 800, "He119");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+
+
+  Raylib.EndDrawing();
+}
