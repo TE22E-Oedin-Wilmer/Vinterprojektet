33ba21df414d8ba545794ef5fbb5f0b9ded890b2
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2023-11-15 13:48:07 +0100

    Jungelbakgrund

--- Vinterprojektet/Program.cs
+++ Vinterprojektet/Program.cs
@@ -4,11 +4,27 @@ using System.Reflection.Metadata;
 using System.Security.Cryptography;
 
 
-Raylib.InitWindow(1000, 800, "He119");
+Raylib.InitWindow(1000, 800, "Gaming");
 Raylib.SetTargetFPS(60);
+
+
+Texture2D jungleBackground = Raylib.LoadTexture("JunglePixel.png");
+
+jungleBackground.Width = 1000;
+jungleBackground.Height = 800;
 
 while (!Raylib.WindowShouldClose())
 {
+
+                          //variabler
+
+
+
+
+Raylib.BeginDrawing();
+Raylib.ClearBackground(Color.GREEN);             //efter börjar allt som inte är variabler
+Raylib.DrawTexture(jungleBackground, 0, 0, Color.WHITE);
+
 
 
   Raylib.EndDrawing();
