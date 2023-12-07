da9e582ca2f73d2a616293e8d935a0c9c6a775e2
Wilmer Ödin <wilmer.odin@elev.ga.ntig.se>
2023-12-07 09:59:42 +0100

    Fullscreen

--- Vinterprojektet/Program.cs
+++ Vinterprojektet/Program.cs
@@ -4,8 +4,14 @@ using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 
+int display = Raylib.GetCurrentMonitor();
 
-Raylib.InitWindow(1000, 800, "Gaming");
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+int monitordisplay = Raylib.GetCurrentMonitor();
+
+Raylib.InitWindow(screenWidth, screenHeight, "Gaming");
 Raylib.SetTargetFPS(60);
 
 int stickx = 200;
@@ -21,16 +27,21 @@ Texture2D StickIdle = Raylib.LoadTexture("StickIdle1.png");
 
 Texture2D StickRun = Raylib.LoadTexture("StickRun1.png");
 
+Texture2D StickRunL = Raylib.LoadTexture("StickRunL1.png");
+
 Texture2D StickJump = Raylib.LoadTexture("StickJump1.png");
 
-jungleBackground.Width = 1000;
-jungleBackground.Height = 800;
+jungleBackground.Width = Raylib.GetMonitorWidth(monitordisplay);
+jungleBackground.Height = Raylib.GetMonitorHeight(monitordisplay);
 
 StickIdle.Width = 200;
 StickIdle.Height = 200;
 
 StickRun.Width = 200;
 StickRun.Height = 200;
+
+StickRunL.Width = 200;
+StickRunL.Height = 200;
 
 StickJump.Width = 200;
 StickJump.Height = 200;
@@ -40,6 +51,27 @@ StickJump.Height = 200;
 
 while (!Raylib.WindowShouldClose())
 {
+
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER) && (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT_ALT) || Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT_ALT)))
+ 		{
+            // see what display we are on right now
+ 			
+ 
+            
+            if (Raylib.IsWindowFullscreen())
+            {
+                // if we are full screen, then go back to the windowed size
+                Raylib.SetWindowSize(screenWidth, screenHeight);
+            }
+            else
+            {
+                // if we are not full screen, set the window size to match the monitor we are on
+                Raylib.SetWindowSize(Raylib.GetMonitorWidth(monitordisplay), Raylib.GetMonitorHeight(monitordisplay));
+            }
+ 
+            // toggle the state
+ 			Raylib.ToggleFullscreen();
+ 		}
 
     Raylib.BeginDrawing();
 
@@ -60,7 +92,7 @@ while (!Raylib.WindowShouldClose())
 
     else if (stickCondition == "runL")
     {
-    Raylib.DrawTexture(StickRun, stickx, sticky, Color.WHITE);
+    Raylib.DrawTexture(StickRunL, stickx, sticky, Color.WHITE);
     stickx --;
     }
 
